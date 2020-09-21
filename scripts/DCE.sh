ca7168bd65799b0a62342096502a35adf1631ee8
Sriramvignesh Mani <sriramvigneshm@midway2-login1.rcc.local>
2020-09-21 17:08:51 -0500

    LAMMPS job

--- scripts/DCE.sh
+++ scripts/DCE.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd test/DCE/
-mpirun -np 4 /home/travis/build/srmani/UCG-GFRD/lammps/src/lmp_mpi -in in.UCG
+mpirun -np 4 /home/travis/build/srmani/UCG-GFRD-Driver/lammps/src/lmp_mpi -in in.UCG
