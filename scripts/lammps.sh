35659dededed62182f3564a8bd5af005a85444e9
Sriramvignesh Mani <m.sriramvignesh@gmail.com>
2020-09-21 11:55:29 -0500

    lammps.sh

--- scripts/lammps.sh
+++ scripts/lammps.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
-git clone -b mdi https://github.com/MolSSI-MDI/lammps.git
-#git clone https://github.com/srmani/UCG-GFRD.git
-pwd
+#git clone -b mdi https://github.com/MolSSI-MDI/lammps.git
+git clone -b stable https://github.com/lammps/lammps.git
 cd lammps
 cd src
-pwd
-#cp ../../UCG-GFRD/src/fix_ucg_gfrd.cpp .
-#cp ../../UCG-GFRD/src/fix_ucg_gfrd.h .
+#cp ../../src/fix_ucg_gfrd.cpp .
+#cp ../../src/fix_ucg_gfrd.h .
 #make lib-mdi args="-m gcc"
 #make yes-user-mdi
 make yes-MOLECULE
