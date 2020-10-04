930220a3244b3197d0799167f6204d63f4540aaf
Sriramvignesh Mani <sriramvigneshm@midway2-login1.rcc.local>
2020-10-03 21:12:29 -0500

    enable mdi option

--- scripts/DCE.sh
+++ scripts/DCE.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 cd test/DCE/
 pwd
-mpirun -np 4 /home/travis/build/srmani/UCG-GFRD-Driver/lammps/src/lmp_mpi -in in.UCG
+#mpirun -np 4 /home/travis/build/srmani/UCG-GFRD-Driver/lammps/src/lmp_mpi -in in.UCG
+mpirun -n 4 /home/travis/build/srmani/UCG-GFRD-Driver/build/UCG-GFRD-Driver/UCG-GFRD-Driver -mdi "-role DRIVER -name driver -method MPI" : -n 4 /home/travis/build/srmani/UCG-GFRD-Driver/lammps/src/lmp_mpi -mdi "-role ENGINE -name LAMMPS -method MPI" -in in.UCG
